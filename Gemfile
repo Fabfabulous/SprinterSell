0c8253aae1d12782e878f7c94802de3cb12dc9ac
Fabien Meuret <fmeuret@gmail.com>
2023-11-28 16:02:54 +0100

    seed and start mapbox

--- Gemfile
+++ Gemfile
@@ -53,6 +53,8 @@ gem "sassc-rails"
 
 gem "geocoder"
 
+gem "faker"
+
 group :development, :test do
   gem "dotenv-rails"
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+company = Company.new(
+  name: "Le Wagon",
+  address: "20 rue des capucins",
+  zip_code: "69001",
+  city: "Lyon",
+  latitude: 45.7694,
+  longitude: 4.8345,
+  status: 1,
+  company_size: 10,
+  code_naf: "8559A"
+)
+company.save!
+
+puts "!!!  START CONTACT CREATION  !!!"
+5.times do
+  contact = Contact.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    function: Faker::Company.profession,
+    email: Faker::Internet.unique.email,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    service: Faker::Company.department,
+    company_id: 1
+  )
+  contact.save!
+end
+puts "!!!  FINISH CONTACT CREATION  !!!"
