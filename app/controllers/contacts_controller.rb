a31dc8068542494f6745ad443df7de67b7ce3d01
Remi Lavigne <remilavigne.pro@gmail.com>
2023-11-28 14:30:43 +0100

    for the login

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,32 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: %i[ show edit update destroy ]
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = contact.new(contact_params)
+    @contact.user = current_user
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :function, :email, :phone_number, :service, :comments)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,4 @@ Rails.application.routes.draw do
   resources :contacts do
     resources :meetings
   end
-
 end
