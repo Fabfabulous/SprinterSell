26f6ed3f5cbf5518c09a11f48f8b745797483821
Fabien Meuret <fmeuret@gmail.com>
2023-11-28 10:46:47 +0100

    route and controller

--- app/models/first_name.rb
+++
@@ -1,3 +0,0 @@
-class FirstName < ApplicationRecord
-  belongs_to :company
-end
--- test/controllers/meetings_controller_test.rb
+++ test/controllers/meetings_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class TitleTest < ActiveSupport::TestCase
+class MeetingsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
