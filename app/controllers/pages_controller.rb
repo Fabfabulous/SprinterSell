25fd9f2d35c741fded37e1bfb2b67d6f42bb31b3
Remi Lavigne <remilavigne.pro@gmail.com>
2023-11-29 15:29:01 +0100

    mapbox created, need fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @companies = Company.all
+    # The `geocoded` scope filters only company with coordinates
+    @markers = @companies.geocoded.map do |company|
+      {
+        lat: company.latitude,
+        lng: company.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { company: company })
+      }
+    end
   end
 end
