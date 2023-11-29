79055520323198c3959f1558955805161d010a4f
Fabien Meuret <fmeuret@gmail.com>
2023-11-29 15:14:35 +0100

    contact display

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @companies_prospect = Company.where(status: 0)
+    @companies_prospect_to_visit = Company.where(status: 2)
   end
 end
