ac24811251bc0abe7494a5118275489147db13af
Fabien Meuret <fmeuret@gmail.com>
2023-12-01 14:08:52 +0100

    Version 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_113827) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_01_114443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_113827) do
     t.string "code_naf"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "note"
   end
 
   create_table "contacts", force: :cascade do |t|
