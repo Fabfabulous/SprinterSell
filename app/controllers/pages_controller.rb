3f28b12208154c3c83d04cf78f4096b62ad98f51
Fabien Meuret <fmeuret@gmail.com>
2023-12-01 10:22:00 +0100

    itineraire

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
 class PagesController < ApplicationController
   def home
     @meetings = Meeting.where("date > ? AND date < ?", DateTime.now.at_beginning_of_day, DateTime.now.end_of_day)
-
     @companies = @meetings.map(&:company)
-
     @markers = @companies.map do |company|
       {
         lat: company.latitude,
@@ -11,9 +9,17 @@ class PagesController < ApplicationController
         info_window_html: render_to_string(partial: "info_window", locals: { company: })
       }
     end
-    # Collect comapnies prospect and propect_to_visit for user's proposal
     @companies_prospect = Company.limit(5).where(status: 0)
     @companies_prospect_to_visit = Company.where(status: 2)
-
+  end
+  def map
+    @companies = Company.all
+    @markers = @companies.map do |company|
+      {
+        lat: company.latitude,
+        lng: company.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { company: })
+      }
+    end
   end
 end
