77531c786c5a659656dd40cfaf27968836a61eab
pierre veillet <pierre.veillet38@gmail.com>
2023-12-06 16:20:20 +0100

    wip

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -13,19 +13,38 @@ class MeetingsController < ApplicationController
   end
 
   def create
+    @user = current_user
     @meeting = Meeting.new(meeting_params)
     duration = meeting_params[:hour]
     @meeting.hour = @meeting.date + duration.to_i.minutes
-    @meeting.user = current_user
+    @meeting.user = @user
+    company = @meeting.company
+    previous_meeting_company = @user.meetings.where('date >= ? AND date < ?', DateTime.current.beginning_of_day, @meeting.date)
+                                     .order(:date)
+                                     .last
+                                     &.company
+    next_meeting_company = @user.meetings.where('date >= ? AND date < ?', @meeting.date, DateTime.current.end_of_day)
+                                         .order(:date)
+                                         .first
+                                         &.company
+
+    coordinates_first = [previous_meeting_company, company].map { |c| { latitude: c.latitude, longitude: c.longitude } }
+    coordinates_last = [company, next_meeting_company].map { |c| { latitude: c.latitude, longitude: c.longitude } }
     # @company = Company.find(params[:id])
     # recuperer une company et lui associer un meeting
+    Mapbox.access_token = ENV["MAPBOX_API_KEY"]
+    duration_itinerary_first = Mapbox::Directions.directions(coordinates_first, "driving-traffic").dig(0, "routes", 0, "duration")&.fdiv(60)&.round if previous_meeting_company
+    duration_itinerary_last = Mapbox::Directions.directions(coordinates_last, "driving-traffic").dig(0, "routes", 0, "duration")&.fdiv(60)&.round if next_meeting_company
+
     sql_query = "date > :start AND date < :end OR hour > :start AND hour < :end"
-    if Meeting.where(sql_query, start: @meeting.date, end: @meeting.hour).empty?
+    if current_user.meetings.where(sql_query, start: @meeting.date, end: @meeting.hour).empty?
       if @meeting.save
         redirect_to root_path
       else
         render :new, status: :unprocessable_entity
       end
+    elsif
+
     else
       redirect_to root_path, notice: "You already have a meeting at this time "
     end
