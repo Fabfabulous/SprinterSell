2a89b3501350463145f0d67dfca957f22fd2bf24
Fabien Meuret <fmeuret@gmail.com>
2023-12-07 16:42:45 +0100

    remove 404/500

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,5 @@ module SprinterSell
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_job.queue_adapter = :sidekiq
-
-    config.exceptions_app = self.routes
-      config.action_dispatch.show_exceptions = true
   end
 end
